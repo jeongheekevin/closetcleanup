13355267176908d7e5afb424cc0ea983dcea3db3
jeongheekevin <jeongheekevin@gmail.com>
2023-07-02 04:22:55 +0900

    modify env

--- closetcleanup/build.gradle.kts
+++ closetcleanup/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.1.2-SNAPSHOT"
+	id("org.springframework.boot") version "2.7.7"
 	id("io.spring.dependency-management") version "1.1.0"
 	kotlin("jvm") version "1.8.22"
 	kotlin("plugin.spring") version "1.8.22"
@@ -25,6 +25,7 @@ repositories {
 	mavenCentral()
 	maven { url = uri("https://repo.spring.io/milestone") }
 	maven { url = uri("https://repo.spring.io/snapshot") }
+	maven { url = uri("https://plugins.gradle.org/m2/") }
 }
 
 dependencies {
