1f674002e1f66f1a1fb723e5a85681d2ae89d24f
jeongheekevin <jeongheekevin@gmail.com>
2023-07-02 04:06:03 +0900

    Initial commit

---
+++ closetcleanup/src/main/kotlin/com/kevin/project/closetcleanup/ClosetcleanupApplication.kt
@@ -0,0 +1,11 @@
+package com.kevin.project.closetcleanup
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClosetcleanupApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClosetcleanupApplication>(*args)
+}
---
+++ closetcleanup/src/test/kotlin/com/kevin/project/closetcleanup/ClosetcleanupApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kevin.project.closetcleanup
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClosetcleanupApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
